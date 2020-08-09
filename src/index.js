793d725e058e9165400027adb4bf6ac4e6cd11ed
Gio Logist <gio@giologist.com>
2020-08-09 09:21:38 -0700

    Multiple Instanes of React

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Find all widget divs
+const WidgetDivs = document.querySelectorAll('.reddit_widget')
+
+// Inject our React App into each
+WidgetDivs.forEach(Div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    Div
+  );
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
